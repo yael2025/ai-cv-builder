3f1b11e4a3b52b35990e7bbdaebdd95de15c184e
yael <yaelpinhas2002@gmail.com>
2026-02-21 19:45:04 +0200

    Adding a button and function to export the resume to a PDF file

--- client/src/pages/Editor.jsx
+++ client/src/pages/Editor.jsx
@@ -3,6 +3,9 @@ import { useCv } from "../context/CvContext";
 import { useNavigate } from "react-router-dom";
 import CvPreviewLive from "../components/CvPreviewLive";
 import { saveCv } from "../services/api";
+import { useRef } from "react";
+import jsPDF from "jspdf"
+import html2canvas from "html2canvas"
 
 
 import ExperienceForm from "../components/ExperienceForm";
@@ -14,6 +17,8 @@ import SkillsForm from "../components/SkillsForm";
 
 
 function Editor(){
+
+  const previewRef = useRef();
 
     
     const navigate = useNavigate();
@@ -30,9 +35,30 @@ function Editor(){
         }
       };
 
+      const handleDownloadPDF = async ()=>{
+        const element = previewRef.current;
+
+        const canvas = await html2canvas(element,{
+          scale:2,
+        });
+
+        const imgData = canvas.toDataURL("image/png")
+
+        const pdf = new jsPDF("p", "mm", "a4");
+
+        const imgWidth = 210;
+        const pageHeight = 297;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+        let position = 0;
+
+        pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+        pdf.save("my-cv.pdf")
+      }
+
     return (
         
-            <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "30px" }}>
+      <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "30px" }}>
     
     {/* Left side – Forms */}
     <div>
@@ -49,10 +75,20 @@ function Editor(){
         >
         Save CV
         </button>
+
+        <button 
+        className="primary"
+        style={{marginTop:"10px"}}
+        onClick={handleDownloadPDF}
+        >
+        Download as PDF
+        </button>
+
+      
     </div>
 
     {/* Right side – Live Preview */}
-    <div className="card">
+    <div className="card" ref={previewRef}>
         <h2>Live Preview</h2>
         <CvPreviewLive />
     </div>
